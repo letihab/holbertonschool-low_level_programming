e865349515c400dfb2f692cbc339cda8f86e12c6
letihab <letihabib1822@gmail.com>
2023-06-20 04:24:12 -0700

    print the number in base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers of base 16 followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+char j;
+for (i = 0; i < 10; i++)
+{
+	putchar((i % 10) + 0));
+}
+for (j = 'a'; j <= 'f'; j++)
+{
+	putchar(j);
+}
+putchar('\n');
+return (0);
+}
