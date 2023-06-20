75520cd37ec939fee4f159ff1c3618eae902259c
letihab <letihabib1822@gmail.com>
2023-06-20 01:55:30 -0700

    lower and upper case alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char i, j;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar('i');
+}
+putchar('\n');
+for (j = 'A'; j <= 'Z'; j++)
+{
+putchar('j');
+}
+putchar('\n');
+return (0);
+}
