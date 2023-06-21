e39cebb2588b9d70c3c146375698416f065f689e
letihab <letihabib1822@gmail.com>
2023-06-21 16:27:41 -0700

    times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,7 +10,25 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-	     putchar('0');
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}~putchar('0');
 
 		for (j = 1; j <= 9; j++)
 		{
