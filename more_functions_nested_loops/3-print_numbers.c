f2d71c1890046068419110c74248c2c596831a97
letihab <letihabib1822@gmail.com>
2023-06-23 05:23:45 -0700

    print number

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -6,12 +6,11 @@
  */
 void print_numbers(void)
 {
-char c = '0';
+char c;
 
-	while (c <= '9')
-	{
-		_putchar(c);
-		c++;
-	}
+for (c = '0'; c <= '9' c++)
+{
+  _putchar(c);
+}
 _putchar('\n');
 }
