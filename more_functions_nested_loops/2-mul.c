897f91ff0b571e39150884a8fe412b7ec9892de1
letihab <letihabib1822@gmail.com>
2023-06-23 04:43:12 -0700

    multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+# include "main.h"
+
+/**
+ *mul - function that multiplies
+ * @a: the int for the parameters of my function
+ * @b: the int for the parameters of my function
+ *Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
