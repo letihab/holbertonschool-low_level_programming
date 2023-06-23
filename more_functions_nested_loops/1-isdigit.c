e3358eb160e68d6d4b80766fb0fb1f36ba1257b8
letihab <letihabib1822@gmail.com>
2023-06-23 04:14:58 -0700

    is digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -7,13 +7,10 @@
  */
 int _isdigit(int c)
 {
-	for (c = '0'; c <= '9'; c++)
+    if (c >= 48 && c <= 57)
 	{
-		if (c >= '0' || c <='9')
-		{
-			return 1;
-		}
-		else
-			return 0;
+	return (1);
 	}
+	else
+	return (0);
 }
