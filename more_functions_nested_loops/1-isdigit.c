e4a00651c2e9f385354079502725d959cac33f32
letihab <letihabib1822@gmail.com>
2023-06-23 04:20:12 -0700

    is digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 # include "main.h"
 
 /**
- *_isdigit _ function that checks for a digit
- *@c: check if c is a digit
- *Return 0
+ * _isdigit - function that checks for uppercase character.
+ * @c: the int for the paramaters of my function
+ * Return: 0
  */
 int _isdigit(int c)
 {
