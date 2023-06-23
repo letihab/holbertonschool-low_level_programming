f575b64b3371307f181997f4ebd0e7703c096118
letihab <letihabib1822@gmail.com>
2023-06-23 13:46:32 -0700

    print line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ *@n : parameter of functions
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
+}
