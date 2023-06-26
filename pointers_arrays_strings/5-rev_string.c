3cf080551dc0f01eac703162813c62091b9c4157
letihab <letihabib1822@gmail.com>
2023-06-26 07:22:12 -0700

    string reverse

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+# include "main.h"
+# include "2-strlen.c"
+
+/**
+ *rev_string - function that prints a string,in reverse
+ *@s: parameter of function
+ *return 0.
+ */
+void rev_string(char *s)
+{
+	int i = _strlen(s) -1;
+	int c;
+
+	for (; i < i/2; i++)
+	{
+		c = s[i];
+		s[i] = s[-i];
+		s[-i] = c;
+	}
+}
