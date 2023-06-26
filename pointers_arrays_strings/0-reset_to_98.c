7685b0e7f1695da0f60ad2cc04bbaa820b8400dd
letihab <letihabib1822@gmail.com>
2023-06-26 04:35:16 -0700

    update pointers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - takes a pointer to an int as parameter and updates value
+ *@n: parameter to the function
+ *return 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
