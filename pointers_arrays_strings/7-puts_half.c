92d51f0cd619572a699360df99b7580b78ba8e93
letihab <letihabib1822@gmail.com>
2023-06-27 06:19:13 -0700

    print array

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,13 +12,37 @@ void puts_half(char *str)
 	int i;
 
 	if (l % 2 == 1)
-                {
-                        l = l + 1;
-		}
-
-	for (i = (l - 1) / 2; i < l ; i++)
+		l = l - 1
+	for (i = l / 2; i < l ; i++)
 	{
 		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+/**void puts_half(char *str)
+{
+	int len, n, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
 	}
 	_putchar('\n');
 }
