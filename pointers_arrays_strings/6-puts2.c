0868e8128e922f6024f2f66ebbe868573eca8b24
letihab <letihabib1822@gmail.com>
2023-06-27 04:47:47 -0700

    print other string

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,15 @@
  */
 void puts2(char *str)
 {
-	int i = _strlen(str);
-	while (str[i] != '\0')
+
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
-		i = i +2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
