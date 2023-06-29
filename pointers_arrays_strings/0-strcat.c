0097e753b6a33dff3ece8783b9a860a44293fc8b
letihab <letihabib1822@gmail.com>
2023-06-29 01:34:20 -0700

    concatenate two string

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -4,28 +4,19 @@
  * _strcat - concatenates n bytes from a string to another
  * @dest: destination string
  * @src: source string
- * @n: number of bytes of str to concatenate
  *
  * Return: a pointer to the resulting string dest
  */
-char *_strcat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	i = 0;
-	j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0' && j < n)
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
-		j++;
 	}
-
-	dest[i] = '\0';
-
 	return (dest);
 }
