734d8c779ec347ada22f154e25bacaf12c4930d6
letihab <letihabib1822@gmail.com>
2023-07-03 05:43:55 -0700

    factorial of a given number

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -9,15 +9,15 @@ int factorial(int n)
 {
 	if (n < 0)
 	{
-		return(- 1);
+		return( -1 );
 	}
 	else if (n > 0)
 	{
 		return(n = n * (n - 1));
+		factorial(n + 1);
 	}
 	else
 		return(1);
-       factorial(n + 1);
 
 	return (0);
 }
