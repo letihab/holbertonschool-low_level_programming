0b3ae347ea78b313a43b81997595268d74f24d8a
letihab <letihabib1822@gmail.com>
2023-07-21 08:23:06 -0700

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,96 +1,47 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
- * printf_char - printfs a char from var args
+ * print_all - function that prints anything
  *
- * @list: va_list to print from
- *
- * Return: void
- */
-void printf_char(va_list list)
-{
-	printf("%c", (char) va_arg(list, int));
-}
-
-/**
- * printf_int - printfs an int from var args
- *
- * @list: va_list to print from
- *
- * Return: void
- */
-void printf_int(va_list list)
-{
-	printf("%d", va_arg(list, int));
-}
-
-/**
- * printf_float - printfs a float from var args
- *
- * @list: va_list to print from
- *
- * Return: void
- */
-void printf_float(va_list list)
-{
-	printf("%f", (float) va_arg(list, double));
-}
-
-/**
- * printf_string - printfs a string from var args
- *
- * @list: va_list to print from
- *
- * Return: void
- */
-void printf_string(va_list list)
-{
-	char *str = va_arg(list, char*);
-
-	while (str != NULL)
-	{
-		printf("%s", str);
-		return;
-	}
-	printf("(nil)");
-}
-
-
-/**
- * print_all - prints various types given a format string for the arguments
- *
- * @format: string containing type information for args
- *
- * Return: void
+ * @format: is a list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	const char *ptr;
 	va_list list;
-	funckey key[4] = { {printf_char, 'c'}, {printf_int, 'i'},
-			   {printf_float, 'f'}, {printf_string, 's'} };
-	int keyind = 0, notfirst = 0;
+	int x = 0;
+	char *k;
+	char *sep = ", ";
 
-	ptr = format;
 	va_start(list, format);
-	while (format != NULL && *ptr)
-	{
-		if (key[keyind].spec == *ptr)
-		{
-			if (notfirst)
-				printf(", ");
-			notfirst = 1;
-			key[keyind].f(list);
-			ptr++;
-			keyind = -1;
-		}
-		keyind++;
-		ptr += keyind / 4;
-		keyind %= 4;
-	}
-	printf("\n");
 
+	while ((format != NULL) && *(format + x) != '\0')
+	{
+		switch (*(format + x))
+		{
+			case 's':  /* string */
+				k = va_arg(list, char *);
+				k = (k != NULL) ? k : "(nil)";
+				printf("%s", k);
+				break;
+			case 'i':  /* int */
+				printf("%i", va_arg(list, int));
+				break;
+			case 'c':  /* char */
+				/* need a cast here since va_arg only takes fully promoted types */
+				printf("%c", va_arg(list, int));
+				break;
+			case 'f':  /* float */
+				printf("%f", va_arg(list, double));
+				break;
+			default:   /* if it does not comply with any of the cases */
+				x++;
+				continue;
+		}
+		if (*(format + x + 1) != 0)
+		{
+			printf("%s", sep);
+		}
+		x++;
+	}
+	putchar(10);
 	va_end(list);
 }
