ffbe0ed261186667198cc37c016d04a44353b843
letihab <letihabib1822@gmail.com>
2023-07-31 14:27:47 -0700

    printed total number of nodes

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,16 +1,17 @@
 #ifndef _LISTS_H
 #define _LISTS_H
+#include <stddef.h>
 
 /**
  *struct list_s singly linked list
  *@n:integer
  *@next: points to the next node
  */
-typedef struct list_s
+typedef struct list_t
 {
 	int n;
-	struct list_s *next;
-} list_s_t;
+	struct list_t *next;
+} list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
