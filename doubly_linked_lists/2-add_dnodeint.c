8f943995fbbfa697d17e8a8c7e7978ccfc57010c
letihab <letihabib1822@gmail.com>
2023-08-02 12:47:41 -0700

    add node at the beginning

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
+
 	new->next = *head;
 	*head = new;
-	return (*head);
+
+	return (new);
 }
