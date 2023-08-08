cf9cc84af6bca58f0730f1261eb314597c3ccddf
letihab <letihabib1822@gmail.com>
2023-08-08 01:44:01 -0700

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-		     len++;
+			len++;
 	}
 	rd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrte = write(rd, text_content, len);
